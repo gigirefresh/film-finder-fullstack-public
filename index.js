7e85b0e135468a184fa6d3145e672d567210a28e
Daniele DalleN <98964578+dannyd2222@users.noreply.github.com>
2025-05-14 15:25:22 +0200

    add express js

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
 console.log("App starting...");
 
 const cowsay = require("cowsay");
+const genres = require('./data/genres.js');
 
-console.log(cowsay.say({
+// console.log(cowsay.say({
+//     text : "Hello Refresh Academy!",
+//     e : "XXX",
+//     T : "V "
+// }));
+
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+//   res.send('Hello World!')
+  const cowSaid = say({
     text : "Hello Refresh Academy!",
     e : "XXX",
     T : "V "
-}));
+  })
+  res.send("<textarea style='height:300px;width:300px'>" + cowSaid + "</textarea>");
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`) // http://localhost:3000
+})
+
+
+console.log();
