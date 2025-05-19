e03306a0153963cde5ab72f31613c6014655d0ce
Daniele DalleN <98964578+dannyd2222@users.noreply.github.com>
2025-05-19 17:19:22 +0200

    cleanup commented out code

--- index.js
+++ index.js
@@ -16,32 +16,24 @@ app.use(express.static("public"));
 
 app.get('/genre/movie/list', (req, res) => {
   console.log(req.query);
-  const dataAsText = fs.readFileSync('data/genres.json', 'utf8');
-  const genres = JSON.parse(dataAsText);
-  res.send(genres)
+  const dataAsJSONText = fs.readFileSync('data/genres.json', 'utf8');
+  const genresObj = JSON.parse(dataAsJSONText);
+  res.send(genresObj)
 })
 
 // https://api.themoviedb.org/3/discover/movie?with_genres=28&api_key=4048775a0f068af3048837ff0341a4f7
 // https://api.themoviedb.org/3/discover/movie?with_genres=99&api_key=4048775a0f068af3048837ff0341a4f7
+
 app.get('/discover/movie', (req, res) => {
   console.log("/discover/movie params: ", req.query)
   // --> stampa  /discover/movie params: { with_genres: '16', api_key: 'ciaomondo' }
 
-  // if (req.query.with_genres==='28') {
-  //   const dataAsText = fs.readFileSync('data/genre-movies-28.json', 'utf8');
-  //   const genreMovies = JSON.parse(dataAsText);
-  //   res.send(genreMovies)
-  // }
-  // if (req.query.with_genres==='99') {
-  //   const dataAsText = fs.readFileSync('data/genre-movies-99.json', 'utf8');
-  //   const genreMovies = JSON.parse(dataAsText);
-  //   res.send(genreMovies)
-  // }
-  const genreId = req.query.with_genres; // <-- SECURITY THREAT!
+  const genreId = req.query.with_genres; 
   const dataAsText = fs.readFileSync(`data/genre-movies-${genreId}.json`, 'utf8');
   const genreMovies = JSON.parse(dataAsText);
-  res.send(genreMovies)
+  res.send(genreMovies);
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`) // http://localhost:3000
