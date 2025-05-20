ad8d1984c806714a7a74816eb1e9c18d80d080a0
Daniele DalleN <98964578+dannyd2222@users.noreply.github.com>
2025-05-20 13:00:13 +0200

    WIP /api/movie/like
    echo service, needs to implement the persistance

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
 console.log("App starting...");
-const cowsay = require("cowsay");
 const express = require('express');
 const fs = require('node:fs');
+const bodyParser = require('body-parser');
 
 const app = express()
 const port = 3000
 app.use(express.static("public"));
-
-
-//// commentin this one so out so the we can default to index.html on /
-// app.get('/', (req, res) => {
-//   res.send("hello world");
-// })
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/genre/movie/list', (req, res) => {
   console.log(req.query);
@@ -34,6 +29,11 @@ app.get('/discover/movie', (req, res) => {
   res.send(genreMovies);
 })
 
+app.post('/api/movie/like', (req, res) => {
+ console.log("Request body: ", req.body);
+ const responseObj = { message: 'Data received successfully', yourData: req.body };
+ res.status(200).json(responseObj);
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`) // http://localhost:3000
